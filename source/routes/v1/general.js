4251bef22f3128a802643c7abbf376f5897e9f7e
MAngelesGB <mgutierrez6@ucol.mx>
2025-02-18 11:53:30 -0600

    Vercel config for deploying, basic structure for PUG views and added a main route

--- source/routes/v1/general.js
+++ source/routes/v1/general.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
+router.get('/',async(req,res)=>{
+  //Redirige a la ruta que seria la principal -->  home
+  res.redirect('/v1/home'); 
+}); 
+
 router.get('/home',async(req,res)=>{
-  res.response('Hello world');
+  res.render('pages/index');
 });
 
 module.exports = router;

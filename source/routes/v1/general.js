c6c3fed53e49bd68429d29b98686c74e95396fb2
MAngelesGB <mgutierrez6@ucol.mx>
2025-02-18 12:24:53 -0600

    vercel config updated to redirect to main route

--- source/routes/v1/general.js
+++ source/routes/v1/general.js
@@ -1,11 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/',async(req,res)=>{
-  //Redirige a la ruta que seria la principal -->  home
-  res.redirect('/v1/home'); 
-}); 
-
 router.get('/home',async(req,res)=>{
   res.render('pages/index');
 });

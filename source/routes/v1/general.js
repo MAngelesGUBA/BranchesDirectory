9c02ffbfc4a7c64ed7023611b45ac93b60449374
MAngelesGB <mgutierrez6@ucol.mx>
2025-02-18 21:33:06 -0600

    config to deploy on vercer updated, part of routes

--- source/routes/v1/general.js
+++ source/routes/v1/general.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
+router.get('/',(req,res)=>{
+  res.redirect('/v1/home'); 
+})
+
 router.get('/home',async(req,res)=>{
   res.render('pages/index');
 });
